7e8a2c90f77795e76f56e1b2b664db65b2a592ca
s-nakajima <nakajimashouhei@gmail.com>
2016-03-28 21:01:53 +0900

    phpunitエラー

--- Test/Case/AllCategoriesTest.php
+++ Test/Case/AllCategoriesTest.php
@@ -2,43 +2,33 @@
 /**
  * Categories All Test Suite
  *
- * @author Ryo Ozawa <ozawa.ryo@withone.co.jp>
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
  * @link http://www.netcommons.org NetCommons Project
  * @license http://www.netcommons.org/license.txt NetCommons License
  * @copyright Copyright 2014, NetCommons Project
  */
 
+App::uses('NetCommonsTestSuite', 'NetCommons.TestSuite');
+
 /**
  * Categories All Test Suite
  *
- * @author Ryo Ozawa <ozawa.ryo@withone.co.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
  * @package NetCommons\Categories\Test\Case
  * @codeCoverageIgnore
  */
-class AllCategoriesTest extends CakeTestSuite {
+class AllCategoriesTest extends NetCommonsTestSuite {
 
 /**
  * All test suite
  *
- * @return CakeTestSuite
+ * @return NetCommonsTestSuite
  */
 	public static function suite() {
 		$plugin = preg_replace('/^All([\w]+)Test$/', '$1', __CLASS__);
-		$suite = new CakeTestSuite(sprintf('All %s Plugin tests', $plugin));
-
-		$directory = CakePlugin::path($plugin) . 'Test' . DS . 'Case';
-		$Folder = new Folder($directory);
-		$exceptions = array(
-			'CategoryTestBase.php',
-			'CategoriesComponentTestBase.php',
-		);
-		$files = $Folder->tree(null, $exceptions, 'files');
-		foreach ($files as $file) {
-			if (substr($file, -4) === '.php') {
-				$suite->addTestFile($file);
-			}
-		}
-
+		$suite = new NetCommonsTestSuite(sprintf('All %s Plugin tests', $plugin));
+		//$suite->addTestDirectoryRecursive(CakePlugin::path($plugin) . 'Test' . DS . 'Case');
 		return $suite;
 	}
 }
