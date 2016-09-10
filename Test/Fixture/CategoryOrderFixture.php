fe0aa894f368a78e7e74ec5a1a7db2fb6bf831a9
s-nakajima <nakajimashouhei@gmail.com>
2016-09-10 17:11:00 +0900

    FixtureのfieldsをShcemaファイルからセットするように修正

--- Test/Fixture/CategoryOrderFixture.php
+++ Test/Fixture/CategoryOrderFixture.php
@@ -15,26 +15,6 @@
 class CategoryOrderFixture extends CakeTestFixture {
 
 /**
- * Fields
- *
- * @var array
- */
-	public $fields = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary', 'comment' => 'ID |  |  | '),
-		'category_key' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'category key | カテゴリーKey | categories.key | ', 'charset' => 'utf8'),
-		'block_key' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'block key | ブロックKey | blocks.key | ', 'charset' => 'utf8'),
-		'weight' => array('type' => 'integer', 'null' => false, 'default' => '0', 'comment' => 'The weight of the display(display order) | 表示の重み(表示順序) |  | '),
-		'created_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'comment' => 'created user | 作成者 | users.id | '),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => 'created datetime | 作成日時 |  | '),
-		'modified_user' => array('type' => 'integer', 'null' => true, 'default' => null, 'comment' => 'modified user | 更新者 | users.id | '),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null, 'comment' => 'modified datetime | 更新日時 |  | '),
-		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1)
-		),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
-	);
-
-/**
  * Records
  *
  * @var array
@@ -82,7 +62,17 @@ class CategoryOrderFixture extends CakeTestFixture {
 			'modified_user' => 1,
 			'modified' => '2015-01-28 04:57:05'
 		),
-
 	);
+
+/**
+ * Initialize the fixture.
+ *
+ * @return void
+ */
+	public function init() {
+		require_once App::pluginPath('Categories') . 'Config' . DS . 'Schema' . DS . 'schema.php';
+		$this->fields = (new CategoriesSchema())->tables[Inflector::tableize($this->name)];
+		parent::init();
+	}
 
 }
